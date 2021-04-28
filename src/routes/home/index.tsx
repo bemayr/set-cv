de02c1bf0bc390c28eedcd5965f439968718cf2a
bemayr <bernhard@mayr.io>
2021-04-28 10:48:54 +0200

    Added first opencv preact rendering example

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,16 +1,16 @@
 import { FunctionalComponent, h } from 'preact';
 import style from './style.css';
 import cv, { Mat, Rect } from "opencv-ts";
+import { useState } from 'preact/hooks';
 
 const Home: FunctionalComponent = () => {
-    const roiRect: Rect = new cv.Rect(0, 0, 200, 200);
-
-    console.log(roiRect)
+    const [rect] = useState(new cv.Rect(0, 0, 200, 200));
 
     return (
         <div class={style.home}>
             <h1>Home</h1>
-            <p>This is the Home component.</p>
+            <p>This is the Home component. And it renders the shape of a Rectangle.</p>
+            <p>{JSON.stringify(rect)}</p>
         </div>
     );
 };

9af40a9e3692d3acbb378e42e576ea0d42d935a6
bemayr <bernhard@mayr.io>
2021-05-25 08:56:10 +0200

    Further Cleanup and Introspection

--- src/components/camera/index.tsx
+++ src/components/camera/index.tsx
@@ -56,6 +56,7 @@ const machine = createMachine({
 
 const SetCamera: FunctionalComponent = () => {
   const webcamRef = useRef(null);
+  const overlayRef = useRef(null);
   const [state, send] = useMachine(machine, {
     services: {
       detect: () => (send) => {
@@ -65,27 +66,128 @@ const SetCamera: FunctionalComponent = () => {
         const dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
         const cap = new cv.VideoCapture(video);
 
-        const FPS = 30;
+        const cnts = new cv.MatVector();
+        const hierarchy: Mat = new cv.Mat();
+
+        let timeout: NodeJS.Timeout;
+
+        const FPS = 2;
         function processVideo() {
           try {
             let begin = Date.now();
             // start processing.
             cap.read(src);
             cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-            cv.imshow("drawing", dst);
+
+            // --- FAR FROM IDEAL ---
+            // cv.GaussianBlur(dst, dst, new cv.Size(1, 1), 1000, 0, cv.BORDER_DEFAULT)
+            // cv.threshold(dst, dst, 120, 255, cv.THRESH_BINARY)
+
+            // --- NOT IDEAL ---
+            // cv.GaussianBlur(dst, dst, new cv.Size(1, 1), 1000, 0, cv.BORDER_DEFAULT)
+            // cv.adaptiveThreshold(dst, dst, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+
+            // // --- C++ Set Solution, not ideal as well ---
+            cv.normalize(dst, dst, 0, 255, cv.NORM_MINMAX);
+            cv.adaptiveThreshold(
+              dst,
+              dst,
+              255,
+              cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+              cv.THRESH_BINARY,
+              181,
+              10
+            );
+
+            // cv.GaussianBlur(
+            //   dst,
+            //   dst,
+            //   new cv.Size(5, 5),
+            //   0,
+            //   0,
+            //   cv.BORDER_DEFAULT
+            // );
+            // cv.threshold(dst, dst, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU);
+
+            const contours = [];
+
+            cv.findContours(
+              dst,
+              // @ts-ignore
+              cnts,
+              hierarchy,
+              cv.RETR_TREE,
+              cv.CHAIN_APPROX_TC89_L1
+            );
+            // @ts-ignore
+            for (let i = 0; i < cnts.size(); ++i) contours.push(cnts.get(i));
+
+            const result = [...contours]
+              .sort((a, b) => cv.contourArea(b) - cv.contourArea(a))
+              .slice(0, 18);
+
+            // const overlay = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+            const overlay = new cv.Mat(src.rows, src.cols, cv.CV_8UC4, new cv.Scalar(0, 0, 0, 0));
+
+            result.forEach((c, i) => {
+              const temp = new cv.MatVector();
+              temp.push_back(c);
+
+              const peri = cv.arcLength(c, true);
+              const approx = new cv.Mat();
+              cv.approxPolyDP(c, approx, 0.04 * peri, true);
+
+              const test = new cv.MatVector();
+              test.push_back(approx);
+
+              // console.log({height: approx.size().height, width: approx.size().width})
+
+              if (approx.size().height === 4) {
+                if (cv.contourArea(c) > 7000) {
+                  cv.drawContours(
+                    overlay,
+                    // @ts-ignore
+                    test,
+                    0,
+                    new cv.Scalar(255, 0, 0, 255),
+                    5,
+                    cv.LINE_8
+                  );
+                } else {
+                  cv.drawContours(
+                    overlay,
+                    // @ts-ignore
+                    test,
+                    0,
+                    new cv.Scalar(0, 255, 0, 255),
+                    5,
+                    cv.LINE_8
+                  );
+                }
+              }
+            });
+
+            // cv.imshow(canvasRef.current!, dst);
+            cv.imshow(overlayRef.current!, overlay);
+
+            overlay.delete();
+
             // schedule the next one.
             let delay = 1000 / FPS - (Date.now() - begin);
-            setTimeout(processVideo, delay);
+            timeout = setTimeout(processVideo, delay);
           } catch (err) {
             console.error(err);
           }
         }
 
-        processVideo();
+        timeout = setTimeout(() => processVideo());
 
         return () => {
+          clearTimeout(timeout);
           src.delete();
           dst.delete();
+          cnts.delete();
+          hierarchy.delete();
         };
       },
     },
@@ -102,91 +204,6 @@ const SetCamera: FunctionalComponent = () => {
     cv.onRuntimeInitialized = () => send("RUNTIME_INITIALIZED");
   });
 
-  // useEffect(() => {
-  //   const worker = setInterval(() => {
-  //     // Take Snapshot
-  //     const imageSrc = webcamRef.current.getScreenshot();
-
-  //     // Create temporary img-element for OpenCV
-  //     let element = document.createElement("img");
-  //     element.src = imageSrc;
-
-  //     setTimeout(() => {
-  //       // @ts-ignore
-  //       const src = cv.imread(element);
-  //       const dst: Mat = new cv.Mat(src.cols, src.rows, cv.CV_8UC4);
-
-  //       cv.cvtColor(src, dst, cv.COLOR_BGR2GRAY);
-
-  //       // --- FAR FROM IDEAL ---
-  //       // cv.GaussianBlur(dst, dst, new cv.Size(1, 1), 1000, 0, cv.BORDER_DEFAULT)
-  //       // cv.threshold(dst, dst, 120, 255, cv.THRESH_BINARY)
-
-  //       // --- NOT IDEAL ---
-  //       // cv.GaussianBlur(dst, dst, new cv.Size(1, 1), 1000, 0, cv.BORDER_DEFAULT)
-  //       // cv.adaptiveThreshold(dst, dst, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-
-  //       // --- C++ Set Solution, not ideal as well ---
-  //       // cv.normalize(dst, dst, 0, 255, cv.NORM_MINMAX)
-  //       // cv.adaptiveThreshold(dst, dst, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 181, 10);
-
-  //       cv.GaussianBlur(dst, dst, new cv.Size(5, 5), 0, 0, cv.BORDER_DEFAULT);
-  //       cv.threshold(dst, dst, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU);
-
-  //       const contours = [];
-  //       const cnts = new cv.MatVector();
-  //       const hierarchy: Mat = new cv.Mat();
-  //       cv.findContours(
-  //         dst,
-  //         // @ts-ignore
-  //         cnts,
-  //         hierarchy,
-  //         cv.RETR_TREE,
-  //         cv.CHAIN_APPROX_TC89_L1
-  //       );
-  //       // @ts-ignore
-  //       for (let i = 0; i < cnts.size(); ++i) contours.push(cnts.get(i));
-
-  //       const result = [...contours]
-  //         .sort((a, b) => cv.contourArea(b) - cv.contourArea(a))
-  //         .slice(0, 18);
-
-  //       let dst1 = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3)
-
-  //       result.forEach((c, i) => {
-  //         const temp = new cv.MatVector()
-  //         temp.push_back(c)
-
-  //         const peri = cv.arcLength(c, true)
-  //         const approx = new cv.Mat();
-  //         cv.approxPolyDP(c, approx, 0.04 * peri, true)
-
-  //         if(approx.size().height === 4) {
-  //           console.log(cv.contourArea(c));
-  //           if(cv.contourArea(c) > 7000) {
-  //             // @ts-ignore
-  //             cv.drawContours(dst1, temp, 0, new cv.Scalar(255, 0, 0), 5, cv.LINE_8)
-  //           }
-  //           else {
-  //             // @ts-ignore
-  //             cv.drawContours(dst1, temp, 0, new cv.Scalar(0, 255, 0), 5, cv.LINE_8)
-  //           }
-  //         }
-  //       });
-
-  //       cv.imshow("drawing", dst1);
-
-  //       src.delete();
-  //       cnts.delete();
-  //       hierarchy.delete();
-  //       dst.delete();
-  //       dst1.delete();
-  //     });
-  //   }, 1000);
-
-  //   return () => clearInterval(worker);
-  // }, []);
-
   return (
     <div>
       <Webcam
@@ -201,10 +218,38 @@ const SetCamera: FunctionalComponent = () => {
         videoConstraints={videoConstraints}
         onUserMedia={() => send("WEBCAM_READY")}
       />
-      <canvas
-        style={{ position: "absolute", opacity: 0.5 }}
-        id="drawing"
-      ></canvas>
+      {/* <canvas
+        ref={overlayRef}
+        style={{ position: "absolute", opacity: 0.95 }}
+      ></canvas> */}
+      <div
+        style={{
+          position: "absolute",
+          bottom: 0,
+          display: "flex",
+          flexDirection: "row",
+          width: "100vw",
+          background: "#00000055",
+          overflowX: "auto",
+          color: "white",
+        }}
+      >
+        <div
+          style={{
+            margin: "10px",
+          }}
+        >
+          <p style={{ margin: "2px" }}>Mask</p>
+          <canvas
+            ref={overlayRef}
+            style={{
+              height: "20vh",
+              minHeight: "150px",
+              border: "1px white solid"
+            }}
+          ></canvas>
+        </div>
+      </div>
       <p style={{ position: "absolute", color: "white" }}>
         {JSON.stringify(state.value, null, 2)}
       </p>

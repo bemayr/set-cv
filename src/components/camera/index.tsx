0830ebc4c0fee3a61d0725c2834135a7ccba9e1e
bemayr <bernhard@mayr.io>
2021-05-26 10:43:47 +0200

    Added @xstate/inspect

--- src/components/camera/index.tsx
+++ src/components/camera/index.tsx
@@ -4,7 +4,7 @@ import { createRef, FunctionalComponent, h } from "preact";
 import { useCallback, useEffect, useRef, useState } from "preact/hooks";
 import { useSizes as useWindowDimensions } from "react-use-sizes";
 import Webcam from "react-webcam";
-import { createMachine, interpret } from "xstate";
+import { assign, createMachine, interpret } from "xstate";
 
 const machine = createMachine({
   id: "toggle",
@@ -36,14 +36,25 @@ const machine = createMachine({
           },
         },
       },
-      onDone: "detecting",
+      onDone: { target: "detecting", actions: ["initialize"] },
     },
     detecting: {
       on: {
         TOGGLE: "paused",
       },
-      invoke: {
-        src: "detect",
+      initial: "idle",
+      states: {
+        idle: {
+          on: {
+            SCAN: "scanning",
+          },
+        },
+        scanning: {
+          invoke: {
+            src: "detect",
+            onDone: "idle",
+          },
+        },
       },
     },
     paused: {
@@ -59,139 +70,152 @@ const SetCamera: FunctionalComponent = () => {
   const thresholdRef = useRef(null);
   const overlayRef = useRef(null);
   const [state, send] = useMachine(machine, {
-    services: {
-      detect: () => (send) => {
+    devTools: true,
+    actions: {
+      initialize: assign(() => {
         // @ts-ignore
         const video = webcamRef.current.base;
         const src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-        const dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
         const cap = new cv.VideoCapture(video);
+        cap.read(src);
+        return { src, cap };
+      }),
+    },
+    services: {
+      detect: (context) => {
+        // @ts-ignore
+        const video = webcamRef.current.base;
+        const src = context.src;
+        const cap = context.cap;
+        const dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
 
         const cnts = new cv.MatVector();
         const hierarchy: Mat = new cv.Mat();
 
-        let timeout: NodeJS.Timeout;
+        cap.read(src);
 
-        const FPS = 30;
-        function processVideo() {
-          try {
-            let begin = Date.now();
-            // start processing.
-            cap.read(src);
-            cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+        // start processing.
+        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
 
-            // --- FAR FROM IDEAL ---
-            // cv.GaussianBlur(dst, dst, new cv.Size(1, 1), 1000, 0, cv.BORDER_DEFAULT)
-            // cv.threshold(dst, dst, 120, 255, cv.THRESH_BINARY)
+        // --- FAR FROM IDEAL ---
+        // cv.GaussianBlur(dst, dst, new cv.Size(1, 1), 1000, 0, cv.BORDER_DEFAULT)
+        // cv.threshold(dst, dst, 120, 255, cv.THRESH_BINARY)
 
-            // --- NOT IDEAL ---
-            cv.GaussianBlur(dst, dst, new cv.Size(1, 1), 1000, 0, cv.BORDER_DEFAULT)
-            cv.adaptiveThreshold(dst, dst, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+        // --- NOT IDEAL ---
+        cv.GaussianBlur(
+          dst,
+          dst,
+          new cv.Size(1, 1),
+          1000,
+          0,
+          cv.BORDER_DEFAULT
+        );
+        cv.adaptiveThreshold(
+          dst,
+          dst,
+          255,
+          cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+          cv.THRESH_BINARY,
+          11,
+          2
+        );
 
-            // // --- C++ Set Solution, not ideal as well ---
-            // cv.normalize(dst, dst, 0, 255, cv.NORM_MINMAX);
-            // cv.adaptiveThreshold(
-            //   dst,
-            //   dst,
-            //   255,
-            //   cv.ADAPTIVE_THRESH_GAUSSIAN_C,
-            //   cv.THRESH_BINARY,
-            //   181,
-            //   10
-            // );
-            // cv.imshow(thresholdRef.current!, dst);
+        // // --- C++ Set Solution, not ideal as well ---
+        // cv.normalize(dst, dst, 0, 255, cv.NORM_MINMAX);
+        // cv.adaptiveThreshold(
+        //   dst,
+        //   dst,
+        //   255,
+        //   cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+        //   cv.THRESH_BINARY,
+        //   181,
+        //   10
+        // );
+        // cv.imshow(thresholdRef.current!, dst);
 
-            // cv.GaussianBlur(
-            //   dst,
-            //   dst,
-            //   new cv.Size(5, 5),
-            //   0,
-            //   0,
-            //   cv.BORDER_DEFAULT
-            // );
-            // cv.threshold(dst, dst, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU);
-            cv.imshow(thresholdRef.current!, dst);
+        // cv.GaussianBlur(
+        //   dst,
+        //   dst,
+        //   new cv.Size(5, 5),
+        //   0,
+        //   0,
+        //   cv.BORDER_DEFAULT
+        // );
+        // cv.threshold(dst, dst, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU);
+        cv.imshow(thresholdRef.current!, src);
 
-            const contours = [];
+        const contours = [];
 
-            cv.findContours(
-              dst,
-              // @ts-ignore
-              cnts,
-              hierarchy,
-              cv.RETR_TREE,
-              cv.CHAIN_APPROX_TC89_L1
-            );
-            // @ts-ignore
-            for (let i = 0; i < cnts.size(); ++i) contours.push(cnts.get(i));
+        cv.findContours(
+          dst,
+          // @ts-ignore
+          cnts,
+          hierarchy,
+          cv.RETR_TREE,
+          cv.CHAIN_APPROX_TC89_L1
+        );
+        // @ts-ignore
+        for (let i = 0; i < cnts.size(); ++i) contours.push(cnts.get(i));
 
-            const result = [...contours]
-              .sort((a, b) => cv.contourArea(b) - cv.contourArea(a))
-              .slice(0, 18);
+        const result = [...contours]
+          .sort((a, b) => cv.contourArea(b) - cv.contourArea(a))
+          .slice(0, 18);
 
-            // const overlay = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-            const overlay = new cv.Mat(src.rows, src.cols, cv.CV_8UC4, new cv.Scalar(0, 0, 0, 0));
+        // const overlay = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+        const overlay = new cv.Mat(
+          src.rows,
+          src.cols,
+          cv.CV_8UC4,
+          new cv.Scalar(0, 0, 0, 0)
+        );
 
-            result.forEach((c, i) => {
-              const temp = new cv.MatVector();
-              temp.push_back(c);
+        result.forEach((c, i) => {
+          const temp = new cv.MatVector();
+          temp.push_back(c);
 
-              const peri = cv.arcLength(c, true);
-              const approx = new cv.Mat();
-              cv.approxPolyDP(c, approx, 0.04 * peri, true);
+          const peri = cv.arcLength(c, true);
+          const approx = new cv.Mat();
+          cv.approxPolyDP(c, approx, 0.04 * peri, true);
 
-              const test = new cv.MatVector();
-              test.push_back(approx);
+          const test = new cv.MatVector();
+          test.push_back(approx);
 
-              // console.log({height: approx.size().height, width: approx.size().width})
+          // console.log({height: approx.size().height, width: approx.size().width})
 
-              if (approx.size().height === 4) {
-                if (cv.contourArea(c) > 7000) {
-                  cv.drawContours(
-                    overlay,
-                    // @ts-ignore
-                    test,
-                    0,
-                    new cv.Scalar(255, 0, 0, 255),
-                    5,
-                    cv.LINE_8
-                  );
-                } else {
-                  cv.drawContours(
-                    overlay,
-                    // @ts-ignore
-                    test,
-                    0,
-                    new cv.Scalar(0, 255, 0, 255),
-                    5,
-                    cv.LINE_8
-                  );
-                }
-              }
-            });
-
-            // cv.imshow(canvasRef.current!, dst);
-            cv.imshow(overlayRef.current!, overlay);
-
-            overlay.delete();
-
-            // schedule the next one.
-            let delay = 1000 / FPS - (Date.now() - begin);
-            timeout = setTimeout(processVideo, delay);
-          } catch (err) {
-            console.error(err);
+          if (approx.size().height === 4) {
+            if (cv.contourArea(c) > 7000) {
+              cv.drawContours(
+                overlay,
+                // @ts-ignore
+                test,
+                0,
+                new cv.Scalar(255, 0, 0, 255),
+                5,
+                cv.LINE_8
+              );
+            } else {
+              cv.drawContours(
+                overlay,
+                // @ts-ignore
+                test,
+                0,
+                new cv.Scalar(0, 255, 0, 255),
+                5,
+                cv.LINE_8
+              );
+            }
           }
-        }
+        });
 
-        timeout = setTimeout(() => processVideo());
+        // cv.imshow(canvasRef.current!, dst);
+        cv.imshow(overlayRef.current!, overlay);
 
-        return () => {
-          clearTimeout(timeout);
-          src.delete();
-          dst.delete();
-          cnts.delete();
-          hierarchy.delete();
-        };
+        overlay.delete();
+        dst.delete();
+        cnts.delete();
+        hierarchy.delete();
+
+        return Promise.resolve();
       },
     },
   });
@@ -207,17 +231,22 @@ const SetCamera: FunctionalComponent = () => {
     cv.onRuntimeInitialized = () => send("RUNTIME_INITIALIZED");
   });
 
+  const FPS = 30;
+  useEffect(() => {
+    const interval = setInterval(() => send("SCAN"), 1000 / FPS);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
       <Webcam
         id="live-video"
         style={{ position: "absolute" }}
-        screenshotFormat="image/jpeg"
         audio={false}
         //@ts-ignore
         ref={webcamRef}
-        width={300}//{windowSize.width}
-        height={200}//{windowSize.height}
+        width={300} //{windowSize.width}
+        height={200} //{windowSize.height}
         videoConstraints={videoConstraints}
         onUserMedia={() => send("WEBCAM_READY")}
       />
@@ -248,7 +277,7 @@ const SetCamera: FunctionalComponent = () => {
             style={{
               height: "20vh",
               minHeight: "150px",
-              border: "1px white solid"
+              border: "1px white solid",
             }}
           ></canvas>
         </div>
@@ -263,20 +292,15 @@ const SetCamera: FunctionalComponent = () => {
             style={{
               height: "20vh",
               minHeight: "150px",
-              border: "1px white solid"
+              border: "1px white solid",
             }}
           ></canvas>
         </div>
       </div>
-      <p style={{ position: "absolute", color: "white" }}>
-        {JSON.stringify(state.value, null, 2)}
-      </p>
-      <button
-        style={{ position: "absolute", color: "white" }}
-        onClick={() => send("TOGGLE")}
-      >
-        Toggle
-      </button>
+      <div style={{ position: "absolute", backgroundColor: "white" }}>
+        <p>{JSON.stringify(state.value, null, 2)}</p>
+        <button onClick={() => send("TOGGLE")}>Pause/Unpause</button>
+      </div>
     </div>
   );
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-import './style/index.css';
-import App from './components/app';
+import "./style/index.css";
+import App from "./components/app";
+import { inspect } from "@xstate/inspect";
+
+if (process.env.NODE_ENV !== "production") {
+  inspect({
+    iframe: false,
+  });
+}
 
 export default App;

f27418654ddb8ef25310c3be884d247d16be4be7
bemayr <bernhard@mayr.io>
2021-06-11 16:41:30 +0200

    Source Format

--- src/types/set.ts
+++ src/types/set.ts
@@ -21,88 +21,98 @@ export function createCard({
   shape: Shape | undefined;
   fill: Fill;
 }): Card | undefined {
-  if (count === undefined || shape === undefined)
-    return undefined;
+  if (count === undefined || shape === undefined) return undefined;
   return { count, color, shape, fill };
 }
 
 const colorToString = {
   green: "G",
   red: "R",
-  purple: "P"
-}
+  purple: "P",
+};
 const shapeToString = {
   oval: "0",
   diamond: "♢",
-  squiggle: "∿"
-}
+  squiggle: "∿",
+};
 const fillToString = {
   blank: "☐",
   striped: "║",
-  solid: "█"
-}
+  solid: "█",
+};
 
 export function cardToString(card: Card): string {
-  return `${colorToString[card.color]}${card.count}${fillToString[card.fill]}${shapeToString[card.shape]}`
+  return `${colorToString[card.color]}${card.count}${fillToString[card.fill]}${
+    shapeToString[card.shape]
+  }`;
 }
-
 
 const colorToNumber = {
   green: 0,
   red: 1,
-  purple: 2
-}
+  purple: 2,
+};
 const shapeToNumber = {
   oval: 0,
   diamond: 1,
-  squiggle: 2
-}
+  squiggle: 2,
+};
 const fillToNumber = {
   blank: 0,
   striped: 1,
-  solid: 2
-}
+  solid: 2,
+};
 
 export default function combinations<T>(collection: T[], n: number): T[][] {
-	let array = collection;
-	if (array.length < n) {
-		return [];
-	}
-	let recur = ((array: T[], n: number) => {
-		if (--n < 0) {
-			return [[]] as T[][];
-		}
-		let combinations = [] as T[][];
-		array = array.slice();
-		while (array.length - n) {
-			let value = array.shift();
-			recur(array, n).forEach((combination: any) => {
-				combination.unshift(value);
-				combinations.push(combination);
-			});
-		}
-		return combinations;
-	});
-	return recur(array, n);
+  if (collection.length < n) return [];
+  let recur = (array: T[], n: number) => {
+    if (--n < 0) return [[]] as T[][];
+    let combinations = [] as T[][];
+    array = array.slice();
+    while (array.length - n) {
+      let value = array.shift();
+      recur(array, n).forEach((combination: any) => {
+        combination.unshift(value);
+        combinations.push(combination);
+      });
+    }
+    return combinations;
+  };
+  return recur(collection, n);
 }
 
 export function isSet(cards: [Card, Card, Card]): boolean {
   const result = cards
-    .map(card => [card.count, colorToNumber[card.color], shapeToNumber[card.shape], fillToNumber[card.fill]])
-    .reduce(([count, color, shape, fill], [currentCount, currentColor, currentShape, currentFill]) =>
-      [count + currentCount, color + currentColor, shape + currentShape, fill + currentFill], [0, 0, 0, 0])
-    .map(sum => sum % 3)
-    .reduce((sum, current) => sum + current, 0)
-    return result === 0;
+    .map((card) => [
+      card.count,
+      colorToNumber[card.color],
+      shapeToNumber[card.shape],
+      fillToNumber[card.fill],
+    ])
+    .reduce(
+      (
+        [count, color, shape, fill],
+        [currentCount, currentColor, currentShape, currentFill]
+      ) => [
+        count + currentCount,
+        color + currentColor,
+        shape + currentShape,
+        fill + currentFill,
+      ],
+      [0, 0, 0, 0]
+    )
+    .map((sum) => sum % 3)
+    .reduce((sum, current) => sum + current, 0);
+  return result === 0;
 }
 
 // def is_set(triplet):
 //     # sum of attributes
 //     sum_attributes = [sum(a) for a in zip(*triplet)]
-    
+
 //     # take modulo 3 of attribute sums
 //     sum_attributes_mod3 = [a % 3 for a in sum_attributes]
-    
+
 //     # all attributes should have a sum of 0
 //     if sum(sum_attributes_mod3) == 0:
 //         return True
